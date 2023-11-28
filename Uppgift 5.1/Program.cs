178f760bfe78a48954effdc731812b2b598e1e8d
Dijar06 <dize06001@utb.vaxjo.se>
2023-11-28 14:54:04 +0100

    Add project files.

---
+++ Uppgift 5.1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Uppgift
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] namn = new string[5];
+            for (int i = 0; i<namn.GetLength(0); i++)
+            {
+                Console.WriteLine("Skriv in namn " + (i + 1) + ":");
+                namn[i] = Console.ReadLine();
+            }
+            for (int i = 0; i<namn.GetLength(0); i++)
+            {
+                Console.WriteLine(namn[i]);
+            }
+        }
+    }
+}
